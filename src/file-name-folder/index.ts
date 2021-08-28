52b59848b3b33fd01268d9699c25a4a74daffa6e
Suhair Zain <suhairzain11@gmail.com>
2021-08-28 14:33:15 +0530

    Fix typo

--- src/file-name-folder/index.ts
+++ src/file-name-folder/index.ts
@@ -7,8 +7,8 @@ const defaultSeparator = sep === "/" ? "\\" : "/";
 
 program
   .version("0.0.1")
-  .requiredOption("-s, --source <file>", "first number")
-  .requiredOption("-t, --target <file>", "second number")
+  .requiredOption("-s, --source <file>", "source directory")
+  .requiredOption("-t, --target <file>", "target directory")
   .option("--separator <value>", "The separator to use", defaultSeparator);
 
 program.parse(process.argv);
